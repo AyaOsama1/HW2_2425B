57174567ade6fd9926a5a30667961cd43ffc4a0c
AyaA117 <a.aya@campus.technion.ac.il>
2025-05-28 19:39:14 +0300

    commit changes

--- dry/main.cpp
+++ dry/main.cpp
@@ -13,11 +13,11 @@ int main() {
     const int crewSize = 2;
     Pirate strawHatCrew[2] = {luffy, zoro};
     // the problem was in line 15 that command called a default constructor in Pirate while Pirate doesn't have one
-    // what I did is : put the values as in assignment and in that way no need to use a default constructor
+    // what I did is : put the values as in assignment and in that way no need to use a default constructor and the
+    // values are in the places that we wanted
     cout << "Straw Hat crew contains:" << endl;
     for (int i = 0; i < crewSize; i++) {
         cout << strawHatCrew[i] << endl;
     }
-
     return 0;
 }
