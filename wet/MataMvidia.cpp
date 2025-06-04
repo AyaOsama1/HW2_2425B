b9db84b504f0c6b13e35aaaf577917a708f141e3
AyaA117 <a.aya@campus.technion.ac.il>
2025-06-04 12:43:24 +0300

    operator +

--- wet/MataMvidia.cpp
+++ wet/MataMvidia.cpp
@@ -82,7 +82,6 @@ MatamMvidia& MataMvidia :: operator[] (int index) {
     return this->frames[index];
 }
 const MatamMvidia& MataMvidia :: operator+ (MataMvidia& other) const {
-   // MataMvidia result = {this -> title, this -> directorName, other.frames + this -> frames, other.frameCount +this -> frameCount};
     MataMvidia result = {*this};
     result += other;
     return result;
